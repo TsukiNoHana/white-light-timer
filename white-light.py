7aedf0d383973eaa6e2c9160f1c9ec40b06498b8
TsukiNoHana <32694409+TsukiNoHana@users.noreply.github.com>
2017-12-11 19:07:26 -0700

    Add files via upload

--- white-light.py
+++ white-light.py
@@ -1,120 +1,76 @@
-import adafruit_ds3231
 import time
-import wiringpi                             #import libraries
+import RPi.GPIO as GPIO                           #import libraries
 
-def is_it_time(store):                      #check the time
-    if time.localtime()==store[0]:
-        if store[0]==True:                  #find the value stored at the list's first value then implement that
-            wiringpi.digitalWrite(pin,1)    #Write HIGH to pin
-        elif store[0]==cycle:
-            cyclical(cycle)
-        elif store[0]==False:
-            wiringpi.digitalWrite(pin,0)    #Write LOW to pin
-        del(store[0])                       #del the first position of store if the time and store matched
-    elif abs(store[0]-time.localtime())>=600:   #if you have ten minutes before the next event, print the menu
-        print_menu()
-    is_it_time(store)                       #run again
-
-def cyclical(cycle):                                                    #when we want to make a repeated action we call this
-    cycles=int((cycle[4]-cycle[0])/(cycle[2]+cycle[3]))                 #find how many cycles there will be from the total run time divided by a single cycle's runtime
+def cyclical(start,start_state,off_len,on_len,end):                                                    #when we want to make a repeated action we call this
+    cycles=int((time.mktime(time.strptime(end,'%d %m %Y %H:%M:%S'))-time.mktime(time.strptime(start,'%d %m %Y %H:%M:%S')))/(on_len+off_len))                 #find how many cycles there will be from the total run time divided by a single cycle's runtime
+    print("Will perform " + str(cycles) + " number of cycles")
     for i in range(cycles):
         if i==0:                                                        #the first cycle should be based on the starting state
-            if cycle[0][1]=='on' or cycle[0][1]=='ON' or cycle[0][1]=='On':
-                wiringpi.digitalWrite(pin,1)  #Write HIGH to pin
-                time.sleep(cycle[0][2])
-                wiringpi.digitalWrite(pin,0)  #Write LOW to pin
-                time.sleep(cycle[0][3])
-            elif cycle[0][1]=='off' or cycle[0][1]=='OFF' or cycle[0][1]=='Off':
-                wiringpi.digitalWrite(pin,0)  #Write LOW to pin
-                time.sleep(cycle[0][3])
-                wiringpi.digitalWrite(pin,1)  #Write HIGH to pin
-                time.sleep(cycle[0][2])
+            print("In first cycle")
+            if start_state=='on' or start_state=='ON' or start_state=='On':
+                print("Start state is on")
+                GPIO.output(pin,1)  #Write HIGH to pin
+                time.sleep(on_len)
+                GPIO.output(pin,0)  #Write LOW to pin
+                time.sleep(off_len)
+            elif start_state=='off' or start_state=='OFF' or start_state=='Off':
+                print("Start state is off")
+                GPIO.output(pin,0)  #Write LOW to pin
+                time.sleep(off_len)
+                GPIO.output(pin,1)  #Write HIGH to pin
+                time.sleep(on_len)
         else:                                                           #there after, just base it on the current state
-            if wiringpi.digitalRead(pin)==1:
-                wiringpi.digitalWrite(pin,0)  #Write LOW to pin
-                time.sleep(cycle[0][3])
-                wiringpi.digitalWrite(pin,1)  #Write HIGH to pin
-                time.sleep(cycle[0][2])
-            elif wiringpi.digitalRead(pin)==0:
-                wiringpi.digitalWrite(pin,1)  #Write HIGH to pin
-                time.sleep(cycle[0][2])
-                wiringpi.digitalWrite(pin,0)  #Write LOW to pin
-                time.sleep(cycle[0][3])
-    del(cycle[0])                                                       #when finished delete the most recent (one just implemented) list of values
-                
-
-def print_menu(store):                                                       #print the menu and send the decision to take_action
+            print("Completed one more cycle")
+            if GPIO.input(pin)==1:
+                GPIO.output(pin,0)  #Write LOW to pin
+                time.sleep(off_len)
+                GPIO.output(pin,1)  #Write HIGH to pin
+                time.sleep(on_len)
+            elif GPIO.input(pin)==0:
+                GPIO.output(pin,1)  #Write HIGH to pin
+                time.sleep(on_len)
+                GPIO.output(pin,0)  #Write LOW to pin
+                time.sleep(off_len)
+    
+def print_menu():                                                       #print the menu and send the decision to take_action
     decision=int(input("""Please Select an Option:
 1. Set time to turn off
 2. Set time to turn on
 3. Set time on/off increments
 4. Turn on
 5. Turn off
-6. Print current plan
-7. Delete current plan
-8. Delete selected time"""))
-    if abs(store[0]-time.localtime())>=600:
-        take_action(store, decision)
-    else:
-        print("Apologies, a shift is currently taking place. Please try again in ten minutes.")
-        is_it_time(store)
+IF EMERGENCY STOP IS REQUIRED USE CTRL + C\n"""))
+    take_action(decision)
 
-def take_action(store, decision):
+def take_action(decision):
     if decision==1:
-        store=store+input("When do you want the light to turn off? [YYYY,MM,DD,HH,MM,SS]")
-        store[(len(store)-1)]=False
-        store.sort()
+        action_time=input("When do you want the light to turn off? DD MM YYYY HH:MM:SS\n")
+        while time.time()<time.mktime(time.strptime(action_time,'%d %m %Y %H:%M:%S')):
+            time.sleep(0.5)
+        GPIO.output(pin,0)
     elif decision==2:
-        store=store+input("When do you want the light to turn on? [YYYY,MM,DD,HH,MM,SS]")
-        store[(len(store)-1)]=True
-        store.sort()
+        action_time=input("When do you want the light to turn on? DD MM YYYY HH:MM:SS\n")
+        while time.time()<time.mktime(time.strptime(action_time,'%d %m %Y %H:%M:%S')):
+            time.sleep(0.5)
+        GPIO.output(pin,1)
     elif decision==3:
-        start_state=input("What do you want the starting state to be? (on/off)")
-        if start_state=='on' or start_state=='ON' or start_state=='On':
-            start=input("When do you want the light to first turn on? [YYYY,MM,DD,HH,MM,SS]")
-            on_len=input("How long do you want the light to be on for in a given cycle in seconds?")
-            off_len=input("How long do you want the light to be off for in a given cycle in seconds?")
-            end=input("When do you want the cycles to end? [YYYY,MM,DD,HH,MM,SS]")
-        elif start_state=='off' or start_state=='OFF' or start_state=='Off':
-            start=input("When do you want the light to first turn off? [YYYY,MM,DD,HH,MM,SS]")
-            off_len=input("How long do you want the light to be off for in a given cycle in seconds?")
-            on_len=input("How long do you want the light to be on for in a given cycle in seconds?")
-            end=input("When do you want the cycles to end? [YYYY,MM,DD,HH,MM,SS]")
-            
-        i=start+start_state+on_len+off_len+end
-        cycle_list.append(i)
-        cycle_list.sort()
-        
-        store=store+start
-        store[(len(store)-1)]='cycle'
-        store.sort()
+        start_state=input("What do you want the starting state to be? (on/off)\n")
+        start=input("When do you want the cycles to start? DD MM YYYY HH:MM:SS\n")
+        off_len=float(input("How long do you want the light to be off for in a given cycle in seconds?\n"))
+        on_len=float(input("How long do you want the light to be on for in a given cycle in seconds?\n"))
+        end=input("When do you want the cycles to end? DD MM YYYY HH:MM:SS\n")
+        while time.time()<time.mktime(time.strptime(start,'%d %m %Y %H:%M:%S')):
+            time.sleep(0.5)
+        print("started cycling")
+        cyclical(start,start_state,off_len,on_len,end)
     elif decision==4:
-        wiringpi.digitalWrite(pin,1)  #Write HIGH to pin
+        GPIO.output(pin,1)  #Write HIGH to pin
     elif decision==5:
-        wiringpi.digitalWrite(pin,0)  #Write LOW to pin
-    elif decision==6:
-        for i in range(len(store)):
-            if store[i]==True:
-                print("The light turns on at", store[i])
-            elif store[i]==False:
-                print("The light turns off at", store[i])
-            elif store[i]=='cycle':
-                print("The light will start cycling at", store[i])
-    elif decision==7:
-        confirmation=input("Are you sure you want to delete the current plan? (y/n)")
+        GPIO.output(pin,0)  #Write LOW to pin
 
-        if confirmation=='y' or confirmation=='Y':
-            store=[]
-    elif decision==8:
-        time_to_delete=input("What is the time of the action you would like to delete? [YYYY,MM,DD,HH,MM,SS]")
-        del(store[time_to_delete])
-        
-    is_it_time(store)
-    
-            
-pin=1
-cycle_list=[]                   #initialize variables
-store=[]
-wiringpi.wiringPiSetupGpio()    #setup gpio interface
-wiringpi.pinMode(pin,1)         #Set GPIO pin to Output
-print_menu(store)               #start running the user interface
+    print_menu()
+
+pin=40
+GPIO.setmode(GPIO.BCM)      #setup pin interface
+GPIO.setup(pin, GPIO.OUT)   #Set GPIO pin to Output
+print_menu()                #start running the user interface
